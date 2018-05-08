952061fdd1823250ceb313f4e7c6d6ba9face255
Aïssa Henni <aissa@appsolute.fr>
2018-05-08 19:11:22 +0200

    - home activity : extract in a separate layout speed portions details view - home activity : adding progress bar

--- app/src/main/java/com/aissah/speedmeter/HomeActivity.kt
+++ app/src/main/java/com/aissah/speedmeter/HomeActivity.kt
@@ -9,7 +9,12 @@ import com.aissah.speedmeter.contract.HomePresenter
 import com.aissah.speedmeter.contract.IHomeContract
 import com.aissah.speedmeter.model.SpeedPortion
 import com.livinglifetechway.quickpermissions.annotations.WithPermissions
+import kotlinx.android.synthetic.main.activity_home.laySpeedPartionDetails
+import kotlinx.android.synthetic.main.activity_home.progressBar
 import kotlinx.android.synthetic.main.activity_home.speedView
+import kotlinx.android.synthetic.main.layout_speedportion_details.tvDistanceTraveled
+import kotlinx.android.synthetic.main.layout_speedportion_details.tvMaxSpeed
+import kotlinx.android.synthetic.main.layout_speedportion_details.tvSpeedAverage
 
 class HomeActivity : AppCompatActivity(), IHomeContract.View {
 
@@ -35,18 +40,32 @@ class HomeActivity : AppCompatActivity(), IHomeContract.View {
 
   override fun onPortionStart() {
     speedView.visibility = View.VISIBLE
+    laySpeedPartionDetails.visibility = View.GONE
+    progressBar.visibility = View.GONE
   }
 
   override fun onPortionEnd() {
     speedView.visibility = View.GONE
+    laySpeedPartionDetails.visibility = View.VISIBLE
     presenter.getLastSpeedPortions()
+    progressBar.visibility = View.VISIBLE
   }
 
   override fun setLastSpeedPortions(portions: List<SpeedPortion>) {
-    TODO("not implemented")
+    //TODO: show a list of all portions
+
+    //for the moment, only show the last one details :
+    val (maxSpeed, averageSpeed, distance) = portions.last()
+    tvMaxSpeed.text = getString(R.string.max_speed, maxSpeed.toString())
+    tvSpeedAverage.text = getString(R.string.speed_average, averageSpeed.toString())
+    tvDistanceTraveled.text = getString(R.string.distance_traveled, distance.toString())
+    progressBar.visibility = View.GONE
   }
 
   override fun setError(errorMessage: String) {
-    Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+    if (errorMessage.isNotEmpty())
+      Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+    else
+      Toast.makeText(this, R.string.error, Toast.LENGTH_SHORT).show()
   }
 }
